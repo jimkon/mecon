2251573268ca5e39f2e1d1cdc206129516526352
jimkon <dkontzedakis@gmail.com>
2022-12-18 20:56:54 +0000

    [BUGFIX]- Zero dates to fill - fill_dates crashed if dataframe had no gaps in days

--- mecon/utils.py
+++ mecon/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from mecon import logs
 
+
 def date_to_month_date(date_series):
     return date_series.dt.year.astype(str) + '-' + date_series.dt.month.astype(str).apply(lambda x: f'{x:0>2}')
 
@@ -15,8 +16,12 @@ def fill_dates(df_in):
 
     existing_dates = set(pd.to_datetime(df_in['date'].unique()))
     all_dates = {df_in['date'].min()+timedelta(days=i) for i in range((df_in['date'].max()-df_in['date'].min()).days+1)}
-    logs.log_calculation(f"Unique dates:{len(existing_dates)}, Time period in days: {len(all_dates)}, min day: {df_in['date'].min()}, max day: {df_in['date'].max()}")
     to_fill_dates = all_dates - existing_dates
+
+    if len(to_fill_dates) == 0:
+        return df_in
+
+    logs.log_calculation(f"Filling {len(to_fill_dates)} days. Unique dates:{len(existing_dates)}, Time period in days: {len(all_dates)}, min day: {df_in['date'].min()}, max day: {df_in['date'].max()}")
     df_to_append = pd.DataFrame({'date': list(to_fill_dates)})
     df_to_append['month_date'] = date_to_month_date(df_to_append['date'])
     df_to_append['time'] = time(0, 0, 0)
