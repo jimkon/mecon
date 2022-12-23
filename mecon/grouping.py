c87f16942477a33dc4f009ce229a1a9231115580
jimkon <dkontzedakis@gmail.com>
2022-12-23 18:39:30 +0000

    [FEATURE]- DataFrame groupers - Mechanism to implement custom groupings for the tagged data

---
+++ mecon/grouping.py
@@ -0,0 +1,54 @@
+import abc
+
+from pandas.core.groupby.generic import DataFrameGroupBy
+
+from mecon.calendar_utils import week_of_month
+
+
+class DataGrouping(DataFrameGroupBy):
+    col_name = None
+
+    def __init__(self, df):
+        df[self.col_name] = self.generate_grouping_column(df)
+        super().__init__(df, keys=self.col_name)
+
+    @abc.abstractmethod
+    def generate_grouping_column(self, df):
+        pass
+
+
+class DailyGrouping(DataGrouping):
+    _col_name = 'date'
+
+    def generate_grouping_column(self, df):
+        return df[self._col_name]
+
+
+class WeeklyGrouping(DataGrouping):
+    col_name = 'year-week'
+
+    def generate_grouping_column(self, df):
+        df['year'] = df['date'].dt.year.astype(str)
+        df['month'] = df['date'].dt.month.apply(lambda x: f"{str(x):0>2}")
+        df['week'] = df['date'].apply(week_of_month).apply(lambda x: f"{str(x):0>2}")
+        df[self.col_name] = df['year'] + '-' + df['month'] + '-' + df['week']
+        return df[self.col_name]
+
+
+class MonthlyGrouping(DataGrouping):
+    col_name = 'year-month'
+
+    def generate_grouping_column(self, df):
+        df['year'] = df['date'].dt.year.astype(str)
+        df['month'] = df['date'].dt.month.apply(lambda x: f"{str(x):0>2}")
+        df[self.col_name] = df['year'] + '-' + df['month']
+        return df[self.col_name]
+
+
+class YearlyGrouping(DataGrouping):
+    col_name = 'year'
+
+    def generate_grouping_column(self, df):
+        df[self.col_name] = df['date'].dt.year.astype(str)
+        return df[self.col_name]
+
