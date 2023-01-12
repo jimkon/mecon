2c69f6d7158bd5aee27afb838cb45863e0be96aa
jimkon <dkontzedakis@gmail.com>
2023-01-12 22:19:48 +0000

    [TAGS] - Guest tag - Golfidis, Sofia, Mama

--- mecon/tagging/tags.py
+++ mecon/tagging/tags.py
@@ -34,9 +34,10 @@ class ITVIncomeTag(HardCodedTag):
         super().__init__('ITV income')
 
     def condition(self, element):
-        return element['amount'] > 2000 and\
-                ('Monzo' in element['tags'] and 'Name:ITV' in element['description'] and 'Category:Income' in element['description']) or \
-                ('HSBC' in element['tags'] and 'ITV PLC CR' in element['description'])
+        return element['amount'] > 2000 and \
+            ('Monzo' in element['tags'] and 'Name:ITV' in element['description'] and 'Category:Income' in element[
+                'description']) or \
+            ('HSBC' in element['tags'] and 'ITV PLC CR' in element['description'])
 
 
 class DeloitteIncomeTag(HardCodedTag):
@@ -44,8 +45,8 @@ class DeloitteIncomeTag(HardCodedTag):
         super().__init__('Deloitte income')
 
     def condition(self, element):
-        return element['amount'] > 2000 and\
-                ('HSBC' in element['tags'] and 'DELOITTE LLP CR' in element['description'])
+        return element['amount'] > 2000 and \
+            ('HSBC' in element['tags'] and 'DELOITTE LLP CR' in element['description'])
 
 
 class IncomeTag(HardCodedTag):
@@ -213,7 +214,7 @@ class CashTag(DictTag):
         ])
 
 
-class CafeTag(DictTag): # TODO
+class CafeTag(DictTag):  # TODO
     def __init__(self):
         super().__init__('Cafe', [
             {'description.lower': {'contains': 'cafe'}},
@@ -222,7 +223,7 @@ class CafeTag(DictTag): # TODO
         ])
 
 
-class FoodOutTag(DictTag): # TODO
+class FoodOutTag(DictTag):  # TODO
     def __init__(self):
         super().__init__('Food out', [
             {'description.lower': {'contains': 'nando'}},
@@ -235,7 +236,7 @@ class FoodOutTag(DictTag): # TODO
         ])
 
 
-class FoodTag(DictTag): # TODO
+class FoodTag(DictTag):  # TODO
     def __init__(self):
         super().__init__('Food', [
             {'tags': {'contains': SuperMarketTag().tag_name}},
@@ -245,7 +246,7 @@ class FoodTag(DictTag): # TODO
         ])
 
 
-class CommuteTag(DictTag): # TODO
+class CommuteTag(DictTag):  # TODO
     def __init__(self):
         super().__init__('Commute', [
             {'tags': {'contains': SantaderBikesTag().tag_name}},
@@ -254,7 +255,7 @@ class CommuteTag(DictTag): # TODO
         ])
 
 
-class DrinksTag(DictTag): # TODO
+class DrinksTag(DictTag):  # TODO
     def __init__(self):
         super().__init__('Drinks', [
             # {'tags': {'contains': SantaderBikesTag().tag_name}},
@@ -262,7 +263,7 @@ class DrinksTag(DictTag): # TODO
         ])
 
 
-class EntertainmentTag(DictTag): # TODO
+class EntertainmentTag(DictTag):  # TODO
     def __init__(self):
         super().__init__('Entertainment', [
             # {'tags': {'contains': SantaderBikesTag().tag_name}},
@@ -283,29 +284,46 @@ class TapTag(DictTag):
 class ChaniaTripTag(DictTag):
     def __init__(self):
         super().__init__('Chania trip', [
-            {'date.str': {'greater_equal': '2019-04-11', 'less': '2020-01-01'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2020-05-16', 'less': '2020-07-01'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2020-07-11', 'less': '2020-09-05'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2021-07-02', 'less': '2021-07-11'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2021-07-22', 'less': '2021-07-09'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2022-01-01', 'less': '2022-01-08'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2022-04-07', 'less': '2022-04-19'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2022-05-25', 'less': '2022-06-04'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2022-08-04', 'less': '2022-09-08'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2019-04-11', 'less': '2020-01-01'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2020-05-16', 'less': '2020-07-01'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2020-07-11', 'less': '2020-09-05'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2021-07-02', 'less': '2021-07-11'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2021-07-22', 'less': '2021-07-09'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2022-01-01', 'less': '2022-01-08'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2022-04-07', 'less': '2022-04-19'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2022-05-25', 'less': '2022-06-04'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2022-08-04', 'less': '2022-09-08'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
         ])
 
 
 class AthensTripTag(DictTag):
     def __init__(self):
         super().__init__('Athens trip', [
-            {'date.str': {'greater_equal': '2020-01-01', 'less': '2020-01-04'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2020-07-02', 'less': '2020-07-11'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2021-07-12', 'less': '2021-07-22'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2021-08-10', 'less': '2021-08-14'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2021-12-14', 'less': '2022-01-01'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2022-01-09', 'less': '2022-01-09'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2022-06-04', 'less': '2022-06-07'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
-            {'date.str': {'greater_equal': '2022-09-08', 'less': '2022-10-12'}, 'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2020-01-01', 'less': '2020-01-04'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2020-07-02', 'less': '2020-07-11'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2021-07-12', 'less': '2021-07-22'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2021-08-10', 'less': '2021-08-14'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2021-12-14', 'less': '2022-01-01'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2022-01-09', 'less': '2022-01-09'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2022-06-04', 'less': '2022-06-07'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
+            {'date.str': {'greater_equal': '2022-09-08', 'less': '2022-10-12'},
+             'tags': {'not_contains': ['Home Bills', 'Income']}},
         ])
 
 
@@ -313,37 +331,37 @@ class BarcelonaTripTag(DictTag):
     def __init__(self):
         super().__init__('Barcelona trip', [{
             'date.str': {
-                'greater_equal': '2022-03-10', # 10am
-                'less_equal': '2022-03-22', # 10am?
-            }, 'tags': {'not_contains': ['Home Bills', 'Income']}},])
+                'greater_equal': '2022-03-10',  # 10am
+                'less_equal': '2022-03-22',  # 10am?
+            }, 'tags': {'not_contains': ['Home Bills', 'Income']}}, ])
 
 
 class BudapestTripTag(DictTag):
     def __init__(self):
         super().__init__('Budapest trip', [{
             'date.str': {
-                'greater_equal': '2022-04-22', # 18:00
-                'less_equal': '2022-04-27', # 6am?
-            }, 'tags': {'not_contains': ['Home Bills', 'Income']}},])
+                'greater_equal': '2022-04-22',  # 18:00
+                'less_equal': '2022-04-27',  # 6am?
+            }, 'tags': {'not_contains': ['Home Bills', 'Income']}}, ])
 
 
 class ParisTripTag(DictTag):
     def __init__(self):
         super().__init__('Paris trip', [{
-                'date.str': {
-                    'greater_equal': '2022-07-07', # apogeuma
-                    'less_equal': '2022-07-15', # prwi
-                }, 'tags': {'not_contains': ['Home Bills', 'Income']}},
+            'date.str': {
+                'greater_equal': '2022-07-07',  # apogeuma
+                'less_equal': '2022-07-15',  # prwi
+            }, 'tags': {'not_contains': ['Home Bills', 'Income']}},
             {
                 'description': {
                     'contains': 'To Ifigeneia Giannoukakou-Leontsini'
                 }
             },
-             {
-                 'description.lower': {
-                     'contains': 'velib metropole'
-                 }
-             },
+            {
+                'description.lower': {
+                    'contains': 'velib metropole'
+                }
+            },
         ])
 
 
@@ -364,98 +382,136 @@ class BankTransferTag(DictTag):
 
 class MondayTag(DictTag):
     def __init__(self):
-        super().__init__(DayOfWeek.MONDAY.value, {'date.dayofweek': {'equals': DayOfWeek.MONDAY.value}, 'tags': {'contains': 'Tap'}})
+        super().__init__(DayOfWeek.MONDAY.value,
+                         {'date.dayofweek': {'equals': DayOfWeek.MONDAY.value}, 'tags': {'contains': 'Tap'}})
 
 
 class TuesdayTag(DictTag):
     def __init__(self):
-        super().__init__(DayOfWeek.TUESDAY.value, {'date.dayofweek': {'equals': DayOfWeek.TUESDAY.value}, 'tags': {'contains': 'Tap'}})
+        super().__init__(DayOfWeek.TUESDAY.value,
+                         {'date.dayofweek': {'equals': DayOfWeek.TUESDAY.value}, 'tags': {'contains': 'Tap'}})
 
 
 class WednesdayTag(DictTag):
     def __init__(self):
-        super().__init__(DayOfWeek.WEDNESDAY.value, {'date.dayofweek': {'equals': DayOfWeek.WEDNESDAY.value}, 'tags': {'contains': 'Tap'}})
+        super().__init__(DayOfWeek.WEDNESDAY.value,
+                         {'date.dayofweek': {'equals': DayOfWeek.WEDNESDAY.value}, 'tags': {'contains': 'Tap'}})
 
 
 class ThursdayTag(DictTag):
     def __init__(self):
-        super().__init__(DayOfWeek.THURSDAY.value, {'date.dayofweek': {'equals': DayOfWeek.THURSDAY.value}, 'tags': {'contains': 'Tap'}})
+        super().__init__(DayOfWeek.THURSDAY.value,
+                         {'date.dayofweek': {'equals': DayOfWeek.THURSDAY.value}, 'tags': {'contains': 'Tap'}})
 
 
 class FridayTag(DictTag):
     def __init__(self):
-        super().__init__(DayOfWeek.FRIDAY.value, {'date.dayofweek': {'equals': DayOfWeek.FRIDAY.value}, 'tags': {'contains': 'Tap'}})
+        super().__init__(DayOfWeek.FRIDAY.value,
+                         {'date.dayofweek': {'equals': DayOfWeek.FRIDAY.value}, 'tags': {'contains': 'Tap'}})
 
 
 class SaturdayTag(DictTag):
     def __init__(self):
-        super().__init__(DayOfWeek.SATURDAY.value, {'date.dayofweek': {'equals': DayOfWeek.SATURDAY.value}, 'tags': {'contains': 'Tap'}})
+        super().__init__(DayOfWeek.SATURDAY.value,
+                         {'date.dayofweek': {'equals': DayOfWeek.SATURDAY.value}, 'tags': {'contains': 'Tap'}})
 
 
 class SundayTag(DictTag):
     def __init__(self):
-        super().__init__(DayOfWeek.SUNDAY.value, {'date.dayofweek': {'equals': DayOfWeek.SUNDAY.value}, 'tags': {'contains': 'Tap'}})
+        super().__init__(DayOfWeek.SUNDAY.value,
+                         {'date.dayofweek': {'equals': DayOfWeek.SUNDAY.value}, 'tags': {'contains': 'Tap'}})
 
+
+class Guest(DictTag):
+    def __init__(self):
+        super().__init__('Guest', [{
+                'date.str': {  # Golfidis
+                    'greater_equal': '2021-10-28',
+                    'less_equal': '2021-11-02',
+                },
+                'tags': {
+                    'not_contains': ['Home Bills', 'Income'],
+                    'contains': 'Monzo'
+                }},
+            {
+                'date.str': {  # Sofia
+                    'greater_equal': '2022-06-14',
+                    'less_equal': '2022-06-28',
+                },
+                'tags': {
+                    'not_contains': ['Home Bills', 'Income'],
+                    'contains': 'Monzo'
+                }},
+            {
+                'date.str': {  # Mama
+                    'greater_equal': '2023-02-02',
+                    'less_equal': '2023-02-06',
+                },
+                'tags': {
+                    'not_contains': ['Home Bills', 'Income'],
+                    'contains': 'Monzo'
+                }}
+        ])
 
 
 BANK_TAGS = [
-MonzoTag(),
-RevolutTag(),
-HSBCTag()
+    MonzoTag(),
+    RevolutTag(),
+    HSBCTag()
 ]
 
 INCOME_TAGS = [
-ITVIncomeTag(),
-DeloitteIncomeTag(),
-IncomeTag(),
+    ITVIncomeTag(),
+    DeloitteIncomeTag(),
+    IncomeTag(),
 ]
 
 SERVICE_TAGS = [
-SpotifyTag(),
-GiffgaffTag(),
-CosmoteTag(),
-SantaderBikesTag(),
-TFLTag(),
-TherapyTag(),
-SuperMarketTag(),
-FoodDeliveryTag(),
-FlightTicketsTag(),
-TrainTicketsTag(),
-RentTag(),
-HomeBillsTag(),
-OtherBillsTag(),
-OnlineOrdersTag(),
-TooGoodToGoTag(),
-CashTag(),
-AirbnbTag(),
-BankTransferTag(),
-CafeTag(),
-FoodOutTag(),
-FoodTag(),
-CommuteTag(),
-DrinksTag(),
-EntertainmentTag(),
-TapTag(),
+    SpotifyTag(),
+    GiffgaffTag(),
+    CosmoteTag(),
+    SantaderBikesTag(),
+    TFLTag(),
+    TherapyTag(),
+    SuperMarketTag(),
+    FoodDeliveryTag(),
+    FlightTicketsTag(),
+    TrainTicketsTag(),
+    RentTag(),
+    HomeBillsTag(),
+    OtherBillsTag(),
+    OnlineOrdersTag(),
+    TooGoodToGoTag(),
+    CashTag(),
+    AirbnbTag(),
+    BankTransferTag(),
+    CafeTag(),
+    FoodOutTag(),
+    FoodTag(),
+    CommuteTag(),
+    DrinksTag(),
+    EntertainmentTag(),
+    TapTag(),
+    Guest(),
 ]
 
 TIME = [
-MondayTag(),
-TuesdayTag(),
-WednesdayTag(),
-ThursdayTag(),
-FridayTag(),
-SaturdayTag(),
-SundayTag(),
+    MondayTag(),
+    TuesdayTag(),
+    WednesdayTag(),
+    ThursdayTag(),
+    FridayTag(),
+    SaturdayTag(),
+    SundayTag(),
 ]
 
 TRIPS = [
-ChaniaTripTag(),
-AthensTripTag(),
-BarcelonaTripTag(),
-BudapestTripTag(),
-ParisTripTag(),
-TripsTag()
+    ChaniaTripTag(),
+    AthensTripTag(),
+    BarcelonaTripTag(),
+    BudapestTripTag(),
+    ParisTripTag(),
+    TripsTag()
 ]
 
 ALL_TAGS = BANK_TAGS + INCOME_TAGS + SERVICE_TAGS + TIME + TRIPS  # order matters
-
