7aef70453716c62b516b59a91bf5b4fc5b3183f2
jimkon <dkontzedakis@gmail.com>
2023-06-24 19:42:35 +0100

    fixed currency rates

---
+++ mecon2/utils/currency.py
@@ -0,0 +1,23 @@
+
+class FixedRateCurrencyConverter:
+    currency_rates = {
+        'USD': 1.3,
+        'EUR': 1.2,
+        'GBP': 1.0
+    }
+
+    def __init__(self, **currency_rates):
+        self._rates = currency_rates if len(currency_rates) else FixedRateCurrencyConverter.currency_rates
+
+    def curr_to_GBP(self, curr):
+        return self._rates[curr]
+
+
+CURRENCY_CONVERTER = FixedRateCurrencyConverter()
+
+
+def currency_rate_function(curr):
+    # raise NotImplemented()
+    return CURRENCY_CONVERTER.curr_to_GBP(curr)
+
+
