b002e0aa733ab218e7ef77d0cb288a7b6bb079ae
jimkon <dkontzedakis@gmail.com>
2023-07-13 19:36:30 +0100

    database data info

--- mecon2/blueprints/data/data_bp.py
+++ mecon2/blueprints/data/data_bp.py
@@ -3,8 +3,11 @@ from typing import Dict
 
 import pandas as pd
 from flask import Blueprint, redirect, url_for, render_template
+from json2html import json2html
 
 from mecon2.app.datasets import current_dataset
+from mecon2.data.db_controller import reset_db, import_data_access, data_access
+from mecon2.data.statements import HSBCStatementCSV, MonzoStatementCSV, RevoStatementCSV
 
 # from mecon2.data import file_system as fs
 
@@ -12,20 +15,82 @@ from mecon2.app.datasets import current_dataset
 data_bp = Blueprint('data', __name__)
 
 
-def _transform_statement_files_dict(files_dict: Dict, dirs_path) -> Dict:
-    transformed_dict = files_dict.copy()
+def _statement_files_info() -> Dict:
+    dirs_path = current_dataset.statements
+    transformed_dict = current_dataset.statement_files().copy()
 
     for dir_name in transformed_dict:
-        transformed_dict[dir_name] = [(pathlib.Path(dirs_path) / dir_name / filename, filename) for filename in transformed_dict[dir_name]]
+        files_info = []
+        for filename in transformed_dict[dir_name]:
+            statement_filepath = pathlib.Path(dirs_path) / dir_name / filename
+            try:
+                df = pd.read_csv(statement_filepath)
+                size = len(df)
+            except FileNotFoundError:
+                size = 'error while reading file'
+            files_info.append((statement_filepath, filename, size))
+        transformed_dict[dir_name] = files_info
 
     return transformed_dict
 
 
+def _db_transactions_info():
+    res = {}
+    hsbc_trans = import_data_access.hsbc_statements.get_transactions()
+    if hsbc_trans is not None:
+        res['HSBC'] = {
+            'transactions': len(hsbc_trans),
+            'days': hsbc_trans['date'].nunique(),
+            'min_date': hsbc_trans['date'].min(),
+            'max_date': hsbc_trans['date'].max(),
+        }
+
+    monzo_trans = import_data_access.monzo_statements.get_transactions()
+    if monzo_trans is not None:
+        res['Monzo'] = {
+            'transactions': len(monzo_trans),
+            'days': monzo_trans['date'].nunique(),
+            'min_date': monzo_trans['date'].min(),
+            'max_date': monzo_trans['date'].max(),
+        }
+
+    revo_trans = import_data_access.revo_statements.get_transactions()
+    if revo_trans is not None:
+        res['Revolut'] = {
+            'transactions': len(revo_trans),
+            'days': revo_trans['start_date'].nunique(),
+            'min_date': revo_trans['start_date'].min(),
+            'max_date': revo_trans['start_date'].max(),
+        }
+
+
+    return res
+
+
 @data_bp.route('/menu')
 def data_menu():
-    menu_dict = _transform_statement_files_dict(current_dataset.statement_files(), current_dataset.statements)
-    return render_template('data.html', menu_dict=menu_dict, **globals())
+    files_info_dict = _statement_files_info()
+    db_transactions_info_dict = json2html.convert(json = _db_transactions_info())
+    return render_template('data.html', **locals(), **globals())
 
+
+@data_bp.post('/reload')
+def data_reload():
+    reset_db()
+    statements_info = _statement_files_info()
+    for bank_name in statements_info:
+        if bank_name == 'HSBC':  # TODO maybe use enums instead of literals
+            dfs = [HSBCStatementCSV(filepath).dataframe() for filepath, *_ in statements_info[bank_name]]
+            import_data_access.hsbc_statements.import_statement(dfs)
+        elif bank_name == 'Monzo':  # TODO maybe use enums instead of literals
+            dfs = [MonzoStatementCSV(filepath).dataframe() for filepath, *_ in statements_info[bank_name]]
+            import_data_access.monzo_statements.import_statement(dfs)
+        elif bank_name == 'Revolut':  # TODO maybe use enums instead of literals
+            dfs = [RevoStatementCSV(filepath).dataframe() for filepath, *_ in statements_info[bank_name]]
+            import_data_access.revo_statements.import_statement(dfs)
+        print(f"{len(dfs)} {bank_name} statements imported")
+
+    return redirect(url_for('data.data_menu'))
 
 @data_bp.post('/import')
 def data_import():
