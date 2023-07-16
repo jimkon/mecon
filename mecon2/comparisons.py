baaecd6a5e1d0a03708b83e96e674f6f60800d22
jimkon <dkontzedakis@gmail.com>
2023-07-16 21:04:55 +0100

    compare operators

--- mecon2/comparisons.py
+++ mecon2/comparisons.py
@@ -1,25 +1,50 @@
+import re
+
+
+class ComparisonFunctionDoesNotExistError(Exception):
+    pass
+
+
+class ComparisonFunctionAlreadyExistError(Exception):
+    pass
 
 
 class CompareOperator:
     """
     Compare operators used by Condition.
     """
+    _instances = {}
+
     def __init__(self, name, function):
         self.name = name
         self.function = function
 
-    def __call__(self, *args, **kwargs):
-        pass
+        if name in self._instances:
+            raise ComparisonFunctionAlreadyExistError
+        self._instances[name] = self
 
-    def apply(self):
-        pass
+    def __call__(self, value_1, value_2):
+        return self.apply(value_1, value_2)
+
+    def apply(self, value_1, value_2):
+        return self.function(value_1, value_2)
 
     def validate(self):
         pass
 
-    @staticmethod
-    def factory_method():
-        pass
+    @classmethod
+    def from_key(cls, key):
+        if key not in cls._instances:
+            raise ComparisonFunctionDoesNotExistError
+        return cls._instances[key]
 
 
-GREATER = CompareOperator('>', lambda a, b: a > b)
+GREATER = CompareOperator('greater', lambda a, b: a > b)
+GREATER_EQUAL = CompareOperator('greater_equal', lambda a, b: a >= b)
+LESS = CompareOperator('less', lambda a, b: a < b)
+LESS_EQUAL = CompareOperator('less_equal', lambda a, b: a <= b)
+EQUAL = CompareOperator('equal', lambda a, b: a == b)
+
+CONTAINS = CompareOperator('contains', lambda a, b: b in a)
+NOT_CONTAINS = CompareOperator('not_contains', lambda a, b: b not in a)
+REGEX = CompareOperator('regex', lambda a, b: bool(re.search(a, b)))
