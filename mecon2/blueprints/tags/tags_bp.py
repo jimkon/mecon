e9ab3ae7ae74e10f1e3648fa39a7923e3c229df4
jimkon <dkontzedakis@gmail.com>
2023-07-24 16:47:16 +0100

    new tag page, save

--- mecon2/blueprints/tags/tags_bp.py
+++ mecon2/blueprints/tags/tags_bp.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template, request
-
+from flask import Blueprint, render_template, request, redirect, url_for
 from mecon2.data.db_controller import data_access
 from mecon2.tagging import Tag
 from mecon2.transactions import Transactions
@@ -13,9 +12,13 @@ def get_transactions() -> Transactions:
     return transactions
 
 
-def render_tag_page(title='Tag page', tag_name='', tag_json_str=None, rename_flag=False):
+def render_tag_page(title='Tag page',
+                    tag_name='',
+                    tag_json_str=None,
+                    rename_flag=False,
+                    rename_button_flag=False,
+                    message_text=''):
     title = 'Create a new tag'
-    rename_flag = False
     tag_name = 'test_tag'
     tag_json_str = '{}' if tag_json_str is None else tag_json_str
     # tag_json_str = '{"amount.int": {"greater": 1000}}'
@@ -34,19 +37,33 @@ def tags_menu():
 
 @tags_bp.route('/new', methods = ['POST', 'GET'])
 def tags_new():
+    rename_flag = True
     if request.method == 'POST':
         if "refresh" in request.form:
             tag_json_str = request.form.get('query_text_input')
         elif "reset" in request.form:
             tag_json_str = None
+        elif "save" in request.form:
+            tag_name = request.form.get('tag_name_input')
+            tag_json_str = request.form.get('query_text_input')
+
+            if data_access.tags.get_tag(tag_name) is not None:
+                message_text = f"Tag '{tag_name}' already exists. Please use another name"
+            else:
+                try:
+                    data_access.tags.set_tag(tag_name, tag_json_str)
+                except Exception as e:
+                    message_text = f"Error: {e}"
+                else:
+                    return redirect(url_for('tags.tag_edit_get', tag_name=tag_name))
     return render_tag_page(title='Create a new tag', **locals())
 
 
 @tags_bp.get('/edit/<tag_name>')
-def tags_edit_get(tag_name):
+def tag_edit_get(tag_name):
     return f'tags_edit_get {tag_name=}'
 
 
 @tags_bp.post('/edit/<tag_name>')
-def tags_edit_post(tag_name):
+def tag_edit_post(tag_name):
     return f'tags_edit_post {tag_name=}'
