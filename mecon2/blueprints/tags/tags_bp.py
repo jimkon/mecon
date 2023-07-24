8e7d83f122456ef53008acca7bc77d646cdb1e11
jimkon <dkontzedakis@gmail.com>
2023-07-24 17:05:37 +0100

    sort and add n_rows capability for tags menu

--- mecon2/blueprints/tags/tags_bp.py
+++ mecon2/blueprints/tags/tags_bp.py
@@ -32,7 +32,9 @@ def render_tag_page(title='Tag page',
 
 @tags_bp.route('/')
 def tags_menu():
-    all_tags = data_access.tags.all_tags()
+    all_tags = sorted(data_access.tags.all_tags(), key=lambda x:x['name'])
+    for tag in all_tags:
+        tag['n_rows'] = 0
     return render_template('tags_menu.html', **locals(), **globals())
 
 
