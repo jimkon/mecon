6c214047fc7eae184517323a591454fb2a9ded71
jimkon <dkontzedakis@gmail.com>
2023-08-18 15:34:54 +0100

    tweaking the graphs in tag_info page

--- mecon2/blueprints/reports/graphs.py
+++ mecon2/blueprints/reports/graphs.py
@@ -9,16 +9,16 @@ def general_cost_stats_html_img(x, y):
     fig, axes = plt.subplots(1, 3, figsize=(10, 4))
 
     timeline_ax, balance_ax, hist_ax = axes
-    timeline_ax.plot(x, y, 'o')
+    timeline_ax.plot(x, y, '.')
     timeline_ax.invert_yaxis()
     timeline_ax.set_title('Timeline')
     # timeline_ax.set_xticklabels(x.to_list(), rotation=45)
 
     balance_ax.plot(x, y.cumsum(), '-.')
-    balance_ax.invert_yaxis()
+    # balance_ax.invert_yaxis()
     balance_ax.set_title('Balance')
 
-    hist_ax.hist(y, bins=30)
+    hist_ax.hist(y, bins='auto')
     hist_ax.invert_xaxis()
     hist_ax.set_title('Histogram')
 
