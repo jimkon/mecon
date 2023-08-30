a4eec0e0caba5781c007652063b1ab7bfeb21a1e
jimkon <dkontzedakis@gmail.com>
2023-08-30 16:37:23 +0100

    changes on multiton, more tests and docs

--- mecon2/utils/multiton.py
+++ mecon2/utils/multiton.py
@@ -7,13 +7,26 @@ class InstanceDoesNotExistError(Exception):
 
 
 class Multiton:
-    _instances = {}
+    """
+    Multiton is a generalised version of a Singleton, that extends to multiple instances
+    that cannot be duplicated. Each instance will be identified and accessed by the
+    instance_name, almost identical to a Flyweight.
 
+    Any subclass of the Multiton class will automatically make the subclass a Multiton.
+    The hierarchical tree beneath the first subclass of the Multiton will belong to
+    the same multiton situation.
+    """
     def __init__(self, instance_name):
-        if instance_name in self._instances:
+        if instance_name in self._get_instances():
             raise InstanceAlreadyExistError
         self._instance_name = instance_name
-        self._instances[self._instance_name] = self
+        self._get_instances()[self._instance_name] = self
+
+    @classmethod
+    def _get_instances(cls):
+        if not hasattr(cls, '_instances'):
+            cls._instances = {}
+        return cls._instances
 
     @property
     def instance_name(self):
@@ -21,11 +34,11 @@ class Multiton:
 
     @classmethod
     def from_key(cls, key):
-        if key not in cls._instances:
+        if key not in cls._get_instances():
             raise InstanceDoesNotExistError(f"Instance '{key=}' does not exist.")
-        return cls._instances[key]
+        return cls._get_instances()[key]
 
     @classmethod
-    def all_transformations(cls):
-        return cls._instances.values()
+    def all_instances(cls):
+        return cls._get_instances().values()
 
--- tests2/unit_tests/test_multiton.py
+++ tests2/unit_tests/test_multiton.py
@@ -3,28 +3,100 @@ import unittest
 from mecon2.utils.multiton import Multiton, InstanceDoesNotExistError, InstanceAlreadyExistError
 
 
-class ExampleMultiton(Multiton):
-    def __init__(self, name, other_args):
-        super().__init__(instance_name=name)
+class TestMultiton(unittest.TestCase):
+    def setUp(self):
+        class DummyMultiton(Multiton):
+            pass
 
+        self.DummyMultiton = DummyMultiton
 
-class TestInstances(unittest.TestCase):
-    def test_instances(self):
-        ExampleMultiton('a', None)
-        ExampleMultiton('b', None)
-        ExampleMultiton('c', None)
+    def test_creation(self):
+        instance_name = "instance1"
+        instance = self.DummyMultiton(instance_name)
+        self.assertEqual(instance._instance_name, instance_name)
 
+    def test_duplicate_creation(self):
+        instance_name = "instance2"
+        self.DummyMultiton(instance_name)
         with self.assertRaises(InstanceAlreadyExistError):
-            ExampleMultiton('a', None)
+            self.DummyMultiton(instance_name)
 
-        self.assertIsNotNone(ExampleMultiton.from_key('a'))
-        self.assertIsNotNone(ExampleMultiton.from_key('b'))
-        self.assertIsNotNone(ExampleMultiton.from_key('c'))
+    def test_from_key_existing(self):
+        instance_name = "instance3"
+        instance = self.DummyMultiton(instance_name)
+        retrieved_instance = self.DummyMultiton.from_key(instance_name)
+        self.assertEqual(instance, retrieved_instance)
+
+    def test_from_key_nonexistent(self):
+        with self.assertRaises(InstanceDoesNotExistError):
+            self.DummyMultiton.from_key("nonexistent_instance")
+
+    def test_all_instances(self):
+        instance1 = self.DummyMultiton("instance4")
+        instance2 = self.DummyMultiton("instance5")
+        all_instances = self.DummyMultiton.all_instances()
+        self.assertIn(instance1, all_instances)
+        self.assertIn(instance2, all_instances)
+
+    def test_different_multiton_subclasses(self):
+        class Subclass1(Multiton):
+            pass
+
+        class Subclass2(Multiton):
+            pass
+
+        instance1_subclass1 = Subclass1("instance1_subclass1")
+        instance2_subclass2 = Subclass2("instance2_subclass2")
+
+        retrieved_instance1_subclass1 = Subclass1.from_key("instance1_subclass1")
+        retrieved_instance2_subclass2 = Subclass2.from_key("instance2_subclass2")
+
+        self.assertEqual(retrieved_instance1_subclass1.instance_name, "instance1_subclass1")
+        self.assertEqual(retrieved_instance2_subclass2.instance_name, "instance2_subclass2")
+
+        # Attempt to access instances from the other subclass
+        with self.assertRaises(InstanceDoesNotExistError):
+            Subclass1.from_key("instance2_subclass2")
 
         with self.assertRaises(InstanceDoesNotExistError):
-            ExampleMultiton.from_key('d')
+            Subclass2.from_key("instance1_subclass1")
 
-        del ExampleMultiton._instances['a']
-        del ExampleMultiton._instances['b']
-        del ExampleMultiton._instances['c']
+        # already existing instances
+        Subclass1("instance2_subclass2")
+        with self.assertRaises(InstanceAlreadyExistError):
+            Subclass1("instance1_subclass1")
 
+        Subclass2("instance1_subclass1")
+        with self.assertRaises(InstanceAlreadyExistError):
+            Subclass2("instance2_subclass2")
+
+    def test_deep_multiton_inheritance(self):
+        class Subclass1(Multiton):
+            pass
+
+        class Subclass11(Subclass1):
+            pass
+
+        class Subclass12(Subclass1):
+            pass
+
+        Subclass1("instance1_subclass1")
+        Subclass11("instance2_subclass11")
+        Subclass11("instance3_subclass12")
+
+        self.assertEqual(Subclass1.from_key("instance1_subclass1").instance_name, "instance1_subclass1")
+        self.assertEqual(Subclass11.from_key("instance2_subclass11").instance_name, "instance2_subclass11")
+        self.assertEqual(Subclass12.from_key("instance3_subclass12").instance_name, "instance3_subclass12")
+
+        self.assertEqual(Subclass1.from_key("instance1_subclass1"), Subclass11.from_key("instance1_subclass1"))
+        self.assertEqual(Subclass1.from_key("instance1_subclass1"), Subclass12.from_key("instance1_subclass1"))
+
+        self.assertEqual(Subclass1.from_key("instance2_subclass11"), Subclass11.from_key("instance2_subclass11"))
+        self.assertEqual(Subclass1.from_key("instance2_subclass11"), Subclass12.from_key("instance2_subclass11"))
+
+        self.assertEqual(Subclass1.from_key("instance3_subclass12"), Subclass11.from_key("instance3_subclass12"))
+        self.assertEqual(Subclass1.from_key("instance3_subclass12"), Subclass12.from_key("instance3_subclass12"))
+
+
+if __name__ == '__main__':
+    unittest.main()
