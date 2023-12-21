b46593116fde623494cac0ec913e686f741de5b4
Dimitrios Kontzedakis <dkontzedakis@gmail.com>
2023-12-21 18:08:11 +0000

    feature: added json links feature in home page

--- mecon/app/views.py
+++ mecon/app/views.py
@@ -1,4 +1,8 @@
+import json
+
 from flask import Blueprint, redirect, url_for, render_template
+
+from mecon.app.datasets import WorkingDatasetDir
 
 main_bp = Blueprint('main', __name__, template_folder='../../templates')
 
@@ -10,6 +14,8 @@ def index():
 
 @main_bp.route('/home')
 def home():
-    return render_template('index.html', **globals())
+    dataset_path = WorkingDatasetDir.get_instance().working_dataset.path
+    links = json.loads((dataset_path / 'links.json').read_text())
+    return render_template('index.html', **locals())
 
 
