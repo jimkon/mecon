5b5bd49613637665d38f26c0ab53253dd56471c5
Dimitrios Kontzedakis <dkontzedakis@gmail.com>
2023-12-26 18:32:35 +0000

    dev feature: dataframe transformer to standardize transformations of dataframes

---
+++ mecon/utils/dataframe_transformers.py
@@ -0,0 +1,24 @@
+import abc
+
+import pandas as pd
+
+
+class DataframeTransformer(abc.ABC):
+    def __init__(self):
+        pass
+
+    def transform(self, df_in: pd.DataFrame) -> pd.DataFrame:
+        self.validate_input_df(df_in)
+        df_out = self._transform(df_in)
+        self.validate_output_df(df_out)
+        return df_out
+
+    @abc.abstractmethod
+    def _transform(self, df) -> pd.DataFrame:
+        pass
+
+    def validate_input_df(self, df: pd.DataFrame):
+        pass
+
+    def validate_output_df(self, df: pd.DataFrame):
+        pass
