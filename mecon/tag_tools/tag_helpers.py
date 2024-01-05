fc38da2153bbbe3bfe8b5cba06655f3ab063f000
Dimitrios Kontzedakis <dkontzedakis@gmail.com>
2024-01-05 16:04:34 +0000

    dev feature: add_rule_for_id will not add the id if it is already in the existing rule

--- mecon/tag_tools/tag_helpers.py
+++ mecon/tag_tools/tag_helpers.py
@@ -4,6 +4,8 @@ from mecon.tag_tools import tagging
 def add_rule_for_id(tag: tagging.Tag, _id_str: str) -> tagging.Tag:
     potential_id_condition = tag.rule.rules[0].rules[0]
     if potential_id_condition.field == 'id' and potential_id_condition.transformation_operation.name == 'none':
+        if _id_str in potential_id_condition.value:
+            return tag
         id_condition = tagging.Condition.from_string_values('id', 'none', 'in',
                                                             f"{_id_str},{potential_id_condition.value}")
         tag.rule.rules[0].rules[0] = id_condition
--- tests/unit_tests/test_tag_helpers.py
+++ tests/unit_tests/test_tag_helpers.py
@@ -4,7 +4,7 @@ from mecon.tag_tools import tag_helpers, tagging
 
 
 class TagHelpersTestCase(unittest.TestCase):
-    def test_add_rule_for_id(self):
+    def test_add_rule_for_id_create_from_scratch(self):
         tag = tagging.Tag.from_json('test',
                                     [{'col1': {'greater': 1}},
                                      {'col1': {'less': -1}}])
@@ -15,8 +15,25 @@ class TagHelpersTestCase(unittest.TestCase):
                               {'col1': {'less': -1}}]
                              )
 
-        new_tag2 = tag_helpers.add_rule_for_id(new_tag, 'test_id2')
-        self.assertListEqual(new_tag2.rule.to_json(),
+    def test_add_rule_for_id_append_in_existing_rule(self):
+        tag = tagging.Tag.from_json('test',
+                                    [{'id': {'in': 'test_id1'}},
+                                     {'col1': {'greater': 1}},
+                                     {'col1': {'less': -1}}])
+        new_tag = tag_helpers.add_rule_for_id(tag, 'test_id2')
+        self.assertListEqual(new_tag.rule.to_json(),
+                             [{'id': {'in': 'test_id2,test_id1'}},
+                              {'col1': {'greater': 1}},
+                              {'col1': {'less': -1}}]
+                             )
+
+    def test_add_rule_for_id_id_already_exists(self):
+        tag = tagging.Tag.from_json('test',
+                                    [{'id': {'in': 'test_id2,test_id1'}},
+                                     {'col1': {'greater': 1}},
+                                     {'col1': {'less': -1}}])
+        new_tag = tag_helpers.add_rule_for_id(tag, 'test_id1')
+        self.assertListEqual(new_tag.rule.to_json(),
                              [{'id': {'in': 'test_id2,test_id1'}},
                               {'col1': {'greater': 1}},
                               {'col1': {'less': -1}}]
