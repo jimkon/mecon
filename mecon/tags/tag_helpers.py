25ccd864ce8ccc0e8b8db9179570cf2f38e0cb0f
jimkon <dkontzedakis@gmail.com>
2024-10-09 12:50:51 +0100

    devfeature: tag_helpers.add_rule_for_id now uses in_csv (in comma separated values) instead of simple in, which is more correct

--- mecon/tags/tag_helpers.py
+++ mecon/tags/tag_helpers.py
@@ -11,11 +11,11 @@ def add_rule_for_id(tag: tagging.Tag, ids_to_add: str | list[str]) -> tagging.Ta
         if len(non_existing_ids_to_add) == 0:
             return tag
         merged_ids_str = ','.join(sorted(non_existing_ids_to_add))
-        id_condition = tagging.Condition.from_string_values('id', 'none', 'in',
+        id_condition = tagging.Condition.from_string_values('id', 'none', 'in_csv',
                                                             f"{merged_ids_str},{potential_id_condition.value}")
         tag.rule.rules[0].rules[0] = id_condition
     else:
         merged_ids_str = ','.join(sorted(ids_to_add))
-        id_condition = tagging.Condition.from_string_values('id', 'none', 'in', merged_ids_str)
+        id_condition = tagging.Condition.from_string_values('id', 'none', 'in_csv', merged_ids_str)
         tag = tagging.Tag(tag.name, tag.rule.append(tagging.Conjunction([id_condition])))
     return tag
--- tests/unit_tests/test_tag_helpers.py
+++ tests/unit_tests/test_tag_helpers.py
@@ -10,43 +10,47 @@ class TagHelpersTestCase(unittest.TestCase):
                                      {'col1': {'less': -1}}])
         new_tag = tag_helpers.add_rule_for_id(tag, 'test_id1')
         self.assertListEqual(new_tag.rule.to_json(),
-                             [{'id': {'in': 'test_id1'}},
+                             [{'id': {'in_csv': 'test_id1'}},
                               {'col1': {'greater': 1}},
                               {'col1': {'less': -1}}]
                              )
 
     def test_add_rule_for_id_append_in_existing_rule(self):
         tag = tagging.Tag.from_json('test',
-                                    [{'id': {'in': 'test_id1'}},
+                                    [{'id': {'in_csv': 'test_id1'}},
                                      {'col1': {'greater': 1}},
                                      {'col1': {'less': -1}}])
         new_tag = tag_helpers.add_rule_for_id(tag, 'test_id2')
         self.assertListEqual(new_tag.rule.to_json(),
-                             [{'id': {'in': 'test_id2,test_id1'}},
+                             [{'id': {'in_csv': 'test_id2,test_id1'}},
                               {'col1': {'greater': 1}},
                               {'col1': {'less': -1}}]
                              )
 
     def test_add_rule_for_id_id_already_exists(self):
         tag = tagging.Tag.from_json('test',
-                                    [{'id': {'in': 'test_id2,test_id1'}},
+                                    [{'id': {'in_csv': 'test_id2,test_id1'}},
                                      {'col1': {'greater': 1}},
                                      {'col1': {'less': -1}}])
         new_tag = tag_helpers.add_rule_for_id(tag, 'test_id1')
         self.assertListEqual(new_tag.rule.to_json(),
-                             [{'id': {'in': 'test_id2,test_id1'}},
+                             [{'id': {'in_csv': 'test_id2,test_id1'}},
                               {'col1': {'greater': 1}},
                               {'col1': {'less': -1}}]
                              )
 
     def test_add_rule_for_id_multiple_ids(self):
         tag = tagging.Tag.from_json('test',
-                                    [{'id': {'in': 'test_id2,test_id1'}},
+                                    [{'id': {'in_csv': 'test_id2,test_id1'}},
                                      {'col1': {'greater': 1}},
                                      {'col1': {'less': -1}}])
         new_tag = tag_helpers.add_rule_for_id(tag, ['test_id1', 'test_id3', 'test_id4'])
         self.assertListEqual(new_tag.rule.to_json(),
-                             [{'id': {'in': 'test_id3,test_id4,test_id2,test_id1'}},
+                             [{'id': {'in_csv': 'test_id3,test_id4,test_id2,test_id1'}},
                               {'col1': {'greater': 1}},
                               {'col1': {'less': -1}}]
                              )
+
+
+if __name__ == '__main__':
+    unittest.main()
