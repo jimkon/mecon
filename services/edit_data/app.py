11a1c8ccce6a52efe3c4e57e03acdf4d46234c37
jimkon <dkontzedakis@gmail.com>
2024-10-09 13:27:49 +0100

    devfeature: editing tag pages

--- services/edit_data/app.py
+++ services/edit_data/app.py
@@ -2,11 +2,13 @@ from starlette.applications import Starlette
 from starlette.routing import Mount
 
 from manual_tagging import manual_tagging_app
+from edit_tags import edit_tags_app
 
 
 # combine apps ----
 routes = [
     Mount('/edit_data/tags/manual', app=manual_tagging_app),
+    Mount('/edit_data/tags/edit', app=edit_tags_app),
 ]
 
 app = Starlette(routes=routes)
