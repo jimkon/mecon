d334036b5372d5bd6959f3c263421c98396916c6
jimkon <dkontzedakis@gmail.com>
2025-01-29 22:57:05 +0000

    dev-feature: changes to dependency graphs, added root groups and table

--- services/reports/tags_info.py
+++ services/reports/tags_info.py
@@ -6,7 +6,7 @@ from shinywidgets import output_widget, render_widget
 
 from mecon import config
 from mecon.app.file_system import WorkingDataManager, WorkingDatasetDir
-from mecon.tags.rule_graphs import TagGraph
+from mecon.tags.rule_graphs import TagGraph, AcyclicTagGraph
 
 # from mecon.monitoring.logs import setup_logging
 
@@ -27,6 +27,18 @@ if dataset is None:
 dm = WorkingDataManager()
 all_tags = dm.all_tags()
 
+tag_graph_all = TagGraph.from_tags(all_tags).remove_cycles()
+tag_roots = tag_graph_all.find_all_root_tags()
+tag_root_groups = {f"root:{tag.name}": f"{tag.name} ({len(tag_graph_all.all_tags_affected_by(tag))})" for tag in tag_roots} # TODO numbers are wrong sometimes
+
+# # tags_group_key = 'Transfers'
+# tags_group_key = 'Train tickets'
+# logging.info(f"{tags_group_key=}", )
+# tg_all = TagGraph.from_tags(all_tags).remove_cycles()
+# subgraph_tags = tg_all.all_tags_affected_by(tags_group_key)
+# logging.info(f"{[tag for tag in subgraph_tags]=}")
+# tg_sub = TagGraph.from_tags(subgraph_tags)
+
 app_ui = ui.page_fluid(
     ui.tags.title("μEcon"),
     ui.navset_pill(
@@ -42,22 +54,22 @@ app_ui = ui.page_fluid(
     ui.layout_sidebar(
         ui.sidebar(
             ui.input_select(id="tags_graph_select", label='Tags group',
-                            choices={'Groups': {'all': 'All'}},
-                            selected='All'),
-            ui.input_checkbox(id="tags_graph_remove_cycles", label='Remove cycles', value=True),
+                            choices={
+                                'Groups': {'group:all': 'All'},
+                                'Roots': tag_root_groups,
+                            },
+                            selected='root:All'),
         ),
-        ui.page_fluid(
-            ui.navset_tab(
-                ui.nav_panel("Dependency Graph",
-                             ui.input_slider(id="tags_graph_k_slider", label='K spring value', min=.05, max=5, value=.5,
-                                             step=.05),
-                             ui.input_checkbox(id="tags_graph_levels", label='Display levels', value=False),
-                             output_widget(id="tags_graph"),
-                             ),
-                ui.nav_panel("Hierarchy Graph",
-
-                             )
-            )
+        ui.navset_tab(
+            ui.nav_panel("Dependency Graph",
+                         # ui.input_slider(id="tags_graph_k_slider", label='K spring value', min=.05, max=5, value=.5,
+                         #                 step=.05),
+                         ui.input_checkbox(id="tags_graph_levels", label='Display levels', value=True),
+                         output_widget(id="tags_graph"),
+                         ),
+            ui.nav_panel("Table",
+                                      ui.output_data_frame('selected_tags_df')
+                         ),
         )
     )
 )
@@ -66,28 +78,41 @@ app_ui = ui.page_fluid(
 def server(input: Inputs, output: Outputs, session: Session):
     @render.text
     def title_output():
-        return f"Tags: {len(all_tags)}"
+        return f"All tags: {len(all_tags)}"
 
     @reactive.calc
     def calc_tags_graph():
-        tags_group_key = input.tags_graph_select()
-        tg = TagGraph.from_tags(all_tags)
-        if input.tags_graph_remove_cycles():
-            tg = TagGraph.from_tags(all_tags).remove_cycles()
+        category, tags_group_key = input.tags_graph_select().split(':')
+        logging.info(f"calc_tags_graph: {category=}, {tags_group_key=}")
+        if category == 'group':
+            tg = TagGraph.from_tags(all_tags)
+            logging.info(f"calc_tags_graph -) {tg.has_cycles()=}")
+            if not tg.has_cycles():
+                tg = tg.remove_cycles()
+                tg.add_hierarchy_levels()
+        elif category == 'root':
+            tg_all = TagGraph.from_tags(all_tags).remove_cycles()
+            subgraph_tags = tg_all.all_tags_affected_by(tags_group_key)
+            logging.info(f"calc_tags_graph -) subgraphs for {tags_group_key} = {[tag for tag in subgraph_tags]=}")
+            tg = AcyclicTagGraph.from_tags(subgraph_tags)
+            tg.add_hierarchy_levels()
+        else:
+            raise ValueError(f"Invalid category: {category}")
 
-        ui.update_checkbox('tags_graph_levels', value=not tg.has_cycles())
+        logging.info(f"calc_tags_graph-> {len(tg.tags)=},{tg.has_cycles()=}")
         return tg
+
+    @render.data_frame
+    def selected_tags_df() -> object:
+        return calc_tags_graph().tidy_table()
 
     @render_widget
     def tags_graph() -> object:
-        logging.info('tags_graph')
         tg = calc_tags_graph()
-
-        if input.tags_graph_remove_cycles():
-            tg = TagGraph.from_tags(all_tags).remove_cycles()
+        logging.info(f"tags_graph: {tg.tags=}")
 
         return tg.create_plotly_graph(
-            k=input.tags_graph_k_slider(),
+            k=.5,  # input.tags_graph_k_slider(),
             levels_col='level' if input.tags_graph_levels() else None,
         )
 
