fba494d8a55d404e4f388fe4a4edfbb8b1c803c2
jimkon <dkontzedakis@gmail.com>
2025-08-29 18:51:13 +0100

    fixed bug with chain of tag lists

--- mecon/tags/process.py
+++ mecon/tags/process.py
@@ -340,9 +340,8 @@ class RuleExecutionPlanTagging(TaggingSession):
                 # # TODO fix: PerformanceWarning: DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
                 # df_in[f"tags_list_{priority}"] = df_in['new_tags_list']
 
-                new_tags_col_series = df_temp.apply(
-                    # DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
-                    lambda row: list(chain(*[row[col] for col in df_temp.columns])), axis=1).rename(new_tags_col)
+                # DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
+                new_tags_col_series = df_temp.apply(lambda row: list(chain(*row.values)), axis=1).rename(new_tags_col)
                 df_in = pd.concat([df_in, new_tags_col_series], axis=1)
                 df_in['new_tags_list'] = df_in.apply(lambda row: row['new_tags_list'] + row[new_tags_col], axis=1)
                 df_in['tags'] = df_in['new_tags_list'].apply(lambda tags: ','.join(tags))
