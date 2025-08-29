248b0783b7a3056e5c0f7b09299998df78362701
jimkon <dkontzedakis@gmail.com>
2025-08-29 18:52:26 +0100

    some more basic tags

--- mecon/data/additional_tags.py
+++ mecon/data/additional_tags.py
@@ -2,6 +2,15 @@ from mecon.tags.tagging import Tag
 from mecon.utils import calendar_utils as cu
 from mecon.etl import account_statements
 
+
+MISC_TAG_SET = {
+    Tag.from_json('Money In', [{"amount": {"greater": 0}}]),
+    Tag.from_json('Zero amount', [{"amount": {"equal": 0}}]),
+    Tag.from_json('Money Out', [{"amount": {"less": 0}}]),
+    Tag.from_json('All', [{"amount": {"greater_equal": 0}},
+                          {"amount": {"less_equal": 0}}]),
+}
+
 TIME_TAG_SET = {
     Tag.from_json('Morning', [{"datetime.hour": {"greater_equal": 8, "less": 12}}]),
     Tag.from_json('Afternoon', [{"datetime.hour": {"greater_equal": 12, "less": 20}}]),
@@ -38,9 +47,9 @@ LOCATION_TAG_SET = {
 class DataProviderTagSet(set):
     def __init__(self, dataset, source_names_to_look_for=None):
         all_statements_sources = account_statements.StatementsManager.from_dataset(dataset,
-                                                                    source_names_to_look_for=source_names_to_look_for)
+                                                                                   source_names_to_look_for=source_names_to_look_for)
         source_tags = {
-            Tag.from_json(source.dir_name, [{'description': {'contains': f"bank:{source.id},"}}])
+            Tag.from_json(f"Source {source.dir_name}", [{'description': {'contains': f"bank:{source.id},"}}])
             for source in all_statements_sources.sources
         }
 
@@ -50,7 +59,7 @@ class DataProviderTagSet(set):
 class HighLevelDataProviderTagSet(set):
     def __init__(self, dataset, source_names_to_look_for=None):
         all_statements_sources = account_statements.StatementsManager.from_dataset(dataset,
-                                                                    source_names_to_look_for=source_names_to_look_for)
+                                                                                   source_names_to_look_for=source_names_to_look_for)
 
         providers_mapping = {}
         for source in all_statements_sources.sources:
@@ -74,11 +83,32 @@ class HighLevelDataProviderTagSet(set):
 
 
 CURRENCY_TAG_SET = {
-    Tag.from_json('GBP', [{"currency": {'equal': 'GBP'}}]),
-    Tag.from_json('EUR', [{"currency": {'equal': 'EUR'}}]),
+    Tag.from_json('£', [{"currency": {'equal': 'GBP'}}]),
+    Tag.from_json('€', [{"currency": {'equal': 'EUR'}}]),
+    Tag.from_json('$', [{"currency": {'equal': 'USD'}}]),
     Tag.from_json('HUF', [{"currency": {'equal': 'HUF'}}]),
     Tag.from_json('RON', [{"currency": {'equal': 'RON'}}]),
     # or use a list of all currencies
 }
 
-CURRENCY_TRANSFER_TAG_SET = {} # check if local currency and amount currency is the same
+CURRENCY_TRANSFER_TAG_SET = {}  # check if local currency and amount currency is the same
+
+PAYMENT_TYPE_TAG_SET = {
+    # online payment
+    # transfers
+    # tap???
+}
+
+
+def get_additional_tags(dataset, source_names_to_look_for=None):
+    all_tags = set()
+    all_tags = all_tags.union(MISC_TAG_SET)
+    all_tags = all_tags.union(TIME_TAG_SET)
+    all_tags = all_tags.union(CALENDAR_WEEK_TAG_SET)
+    all_tags = all_tags.union(LOCATION_TAG_SET)
+    all_tags = all_tags.union(DataProviderTagSet(dataset))
+    all_tags = all_tags.union(HighLevelDataProviderTagSet(dataset))
+    all_tags = all_tags.union(CURRENCY_TAG_SET)
+    all_tags = all_tags.union(CURRENCY_TRANSFER_TAG_SET)
+
+    return all_tags
