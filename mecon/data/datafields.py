754a68dd81038d0ff207554afc14ac71772c2959
jimkon <dkontzedakis@gmail.com>
2025-08-29 18:51:28 +0100

    minor: add monitor

--- mecon/data/datafields.py
+++ mecon/data/datafields.py
@@ -368,12 +368,14 @@ class TagsColumnMixin(ColumnMixin):
         tagging.Tagger.tag(tag, new_df)
         return self._df_wrapper_obj.factory(new_df)
 
-    def apply_tags(self, tags: Iterable[tagging.Tag]) -> DataframeWrapper:
+    def apply_tags(self,
+                   tags: Iterable[tagging.Tag],
+                   monitor=None) -> DataframeWrapper:
         from mecon.tags.process import OptREPTagging
         sess = OptREPTagging(tags) \
             .create_rule_execution_plan() \
             .create_optimised_rule_execution_plan()
-        tx_tagged = sess.tag(self, monitor=None)
+        tx_tagged = sess.tag(self, monitor=monitor)
         return tx_tagged
 
 
