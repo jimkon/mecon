a0c586d3967027b48732163aa4336649aa9e8bc9
jimkon <dkontzedakis@gmail.com>
2025-10-21 20:15:33 +0100

    minor change on error logs

--- mecon/app/shiny_app.py
+++ mecon/app/shiny_app.py
@@ -241,13 +241,13 @@ def filter_funcs_factory(
         transactions = data_manager.get_transactions()
         filtered_in_transactions = transactions.containing_tags(filter_in_tags)
         if filtered_in_transactions.size() == 0:
-            error_msg = f"No transactions found for {filter_url_params['time_unit']} time unit containing {filter_url_params['filter_in_tags']} tags."
+            error_msg = f"No transactions found containing {filter_url_params['filter_in_tags']} tags."
             raise ShinyTransactionFilterError(error_msg)
 
         filtered_in_and_out_transactions = filtered_in_transactions.not_containing_tags(filter_out_tags,
                                                                                         empty_tags_strategy='all_true')
         if filtered_in_and_out_transactions.size() == 0:
-            error_msg = f"No transactions found for {filter_url_params['time_unit']} time unit after filtering out {filter_url_params['filter_in_tags']} tags."
+            error_msg = f"No transactions found after filtering out {filter_url_params['filter_in_tags']} tags."
             raise ShinyTransactionFilterError(error_msg)
 
         logging.info(f"URL param transactions: {filtered_in_and_out_transactions.size()=}")
